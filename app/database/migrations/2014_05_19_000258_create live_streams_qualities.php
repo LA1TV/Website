249147ce255aec684331fbca57ade1da8e718951
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-19 00:08:48 +0100

    create live_stream_qualities

---
+++ app/database/migrations/2014_05_19_000258_create live_streams_qualities.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLiveStreamsQualities extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('live_streams_qualities', function(Blueprint $table)
+		{
+			$table->integer("live_stream_id")->unsigned();
+			$table->string("quality_id", 20);
+			$table->primary(array("live_stream_id", "quality_id"));
+			$table->tinyInteger("position")->unsigned();
+			$table->timestamps();
+			
+			$table->index("live_stream_id");
+			
+			$table->foreign("live_stream_id")->references('id')->on('live_streams')->onUpdate("restrict")->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('live_streams_qualities');
+	}
+
+}
