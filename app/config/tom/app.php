1abdd518ba8268c7113fb43569ecec353ee5ecd8
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-18 16:55:44 +0100

    added "tom" custom config

--- app/config/tom/app.php
+++ app/config/tom/app.php
@@ -14,5 +14,6 @@ return array(
 	*/
 
 	'debug' => true,
+	'url' => 'http://127.0.0.1/la1tv',
 
 );
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'tom' => array('Tom-Lenovo'),
 
 ));
 
