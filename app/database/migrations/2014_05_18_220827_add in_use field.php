8ba2c15d6218b4a6dc1b1931180666705bbd5f62
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-18 22:09:58 +0100

    add in_use field to files table

---
+++ app/database/migrations/2014_05_18_220827_add in_use field.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddInUseField extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('files', function(Blueprint $table)
+		{
+			$table->boolean("in_use")->default(FALSE);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('files', function(Blueprint $table)
+		{
+			$table->dropColumn("in_use");
+		});
+	}
+
+}
