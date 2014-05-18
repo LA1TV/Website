a8f47b52f8e540888e1af2a6246e5340ba01bd86
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-18 21:13:45 +0100

    create series table

---
+++ app/database/migrations/2014_05_18_211318_create series table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('series', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 50);
+			$table->string('description')->nullable();
+			$table->boolean('cover_file_id')->nullable();
+			$table->boolean('side_banner_file_id')->nullable();
+			$table->boolean('enabled');
+			$table->timestamp('scheduled_publish_time')->nullable();
+			$table->timestamps();
+			
+			$table->foreign("cover_file_id")->references('id')->on('files')->onUpdate("restrict")->onDelete('set null');
+			$table->foreign("side_banner_file_id")->references('id')->on('files')->onUpdate("restrict")->onDelete('set null');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('series');
+	}
+
+}
