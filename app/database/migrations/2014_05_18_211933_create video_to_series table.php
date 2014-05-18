0a2fe6b4f4c9c910fdf7e460625a45814179adde
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-18 21:23:37 +0100

    created video_to_series table

---
+++ app/database/migrations/2014_05_18_211933_create video_to_series table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoToSeriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('video_to_series', function(Blueprint $table)
+		{
+			$table->integer("video_id")->unsigned();
+			$table->integer("series_id")->unsigned();
+			$table->primary(array("video_id", "series_id"));
+			$table->smallInteger("position")->unsigned();
+			$table->timestamps();
+			
+			$table->foreign("video_id")->references('id')->on('videos')->onUpdate("restrict")->onDelete('cascade');
+			$table->foreign("series_id")->references('id')->on('series')->onUpdate("restrict")->onDelete('cascade');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('video_to_series');
+	}
+
+}
