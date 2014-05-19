08d125387573b27eccd3b8a8b8871e2873fb44ff
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-19 15:24:42 +0100

    migrations fix

--- app/database/migrations/2014_05_19_141150_rename series_id to playlist_id.php
+++ app/database/migrations/2014_05_19_141150_rename series_id to playlist_id.php
@@ -14,7 +14,13 @@ class RenameSeriesIdToPlaylistId extends Migration {
 	{
 		Schema::table('video_to_playlist', function(Blueprint $table)
 		{
+			$table->dropForeign('video_to_series_video_id_foreign');
+			$table->dropForeign('video_to_series_series_id_foreign');
+			
 			$table->renameColumn('series_id', 'playlist_id');
+			
+			$table->foreign("video_id")->references('id')->on('videos')->onUpdate("restrict")->onDelete('cascade');
+			$table->foreign("series_id")->references('id')->on('series')->onUpdate("restrict")->onDelete('cascade');
 		});
 	}
 
@@ -27,7 +33,13 @@ class RenameSeriesIdToPlaylistId extends Migration {
 	{
 		Schema::table('video_to_playlist', function(Blueprint $table)
 		{
+			$table->dropForeign('video_to_playlist_video_id_foreign');
+			$table->dropForeign('video_to_playlist_series_id_foreign');
+			
 			$table->renameColumn('playlist_id', 'series_id');
+			
+			$table->foreign("video_id")->references('id')->on('videos')->onUpdate("restrict")->onDelete('cascade');
+			$table->foreign("series_id")->references('id')->on('series')->onUpdate("restrict")->onDelete('cascade');
 		});
 	}
 
