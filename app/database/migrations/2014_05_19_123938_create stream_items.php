eefd5ccb888f5d7b7bf4b13ad6b42b65ac03c952
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-19 12:43:35 +0100

    create stream_items

---
+++ app/database/migrations/2014_05_19_123938_create stream_items.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStreamItems extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('stream_items', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->boolean("enabled");
+			$table->integer('video_id')->unsigned()->nullable();
+			$table->integer('live_stream_id')->unsigned()->nullable();
+			$table->timestamps();
+			
+			$table->index("video_id");
+			$table->index("live_stream_id");
+			
+			$table->foreign("video_id")->references('id')->on('videos')->onUpdate("restrict")->onDelete('cascade');
+			$table->foreign("live_stream_id")->references('id')->on('live_streams')->onUpdate("restrict")->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('stream_items');
+	}
+
+}
