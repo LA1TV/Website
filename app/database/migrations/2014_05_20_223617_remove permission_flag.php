52ff0b4fab28617d2a8154d4597e3916f4bc899e
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-20 22:37:18 +0100

    remove permission_flag

---
+++ app/database/migrations/2014_05_20_223617_remove permission_flag.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemovePermissionFlag extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('permission_to_group', function(Blueprint $table)
+		{
+			$table->dropColumn('permission_flag');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('permission_to_group', function(Blueprint $table)
+		{
+			$table->boolean('permission_flag');
+		});
+	}
+
+}
