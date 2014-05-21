14eb9970ad5fefbf76695a9cabc681f875cd6c59
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-21 11:03:06 +0100

    added save validation check to LiveStream

--- app/models/LiveStream.php
+++ app/models/LiveStream.php
@@ -7,6 +7,19 @@ class LiveStream extends Eloquent {
 	protected $table = 'live_streams';
 	protected $fillable = array('name', 'description', 'load_balancer_server_address', 'server_address', 'dvr_enabled');
 	
+	public function __construct(array $attributes = array()) {
+		parent::__construct($attributes);
+		self::saving(function($model) {
+			if ($model->load_balancer_server_address === NULL && $model->server_address === NULL) {
+				throw(new Exception("Either 'load_balancer_server_address' or 'server_address' must be set."));
+			}
+			else if ($model->load_balancer_server_address !== NULL && $model->server_address !== NULL) {
+				throw(new Exception("Only one of 'load_balancer_server_address' or 'server_address' must be set."));
+			}
+			return true;
+		});
+	}
+	
 	public function qualities() {
 		return $this->hasMany('LiveStreamQuality', 'live_stream_id');
 	}
