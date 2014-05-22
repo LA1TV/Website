69e061cdbd31a365622d3944a1412dd18226c00a
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-23 00:43:44 +0100

    attempted fix

--- app/models/MyEloquent.php
+++ app/models/MyEloquent.php
@@ -10,17 +10,17 @@ class MyEloquent extends Eloquent {
 	
 	public function push() {
 		$returnVal = NULL;
-		DB::transaction(function() use (&$returnVal) {
+//		DB::transaction(function() use (&$returnVal) {
 			$returnVal = parent::push();
-		});
+//		});
 		return $returnVal;
 	}
 	
 	public function save(array $options = array()) {
 		$returnVal = NULL;
-		DB::transaction(function() use (&$options, &$returnVal) {
+//		DB::transaction(function() use (&$options, &$returnVal) {
 			$returnVal = parent::save($options);
-		});
+//		});
 		return $returnVal;
 	}
 }
