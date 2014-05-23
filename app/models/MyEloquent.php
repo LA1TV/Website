939f9f4d075e8d97b8a6ee46c75f3d7b7aa59eff
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-23 01:17:49 +0100

    php parent:: bug workaround

--- app/models/MyEloquent.php
+++ app/models/MyEloquent.php
@@ -8,19 +8,34 @@ class MyEloquent extends Eloquent {
 	
 	protected static $p = "uk\\co\\la1tv\\website\\models\\";
 	
+	// can't use $transaction because cause php error on server for some reason when trying to access parent:: from closure
+	// Cannot access parent:: when no class scope is active
+	
 	public function push() {
 		$returnVal = NULL;
-		DB::transaction(function() use (&$returnVal) {
+		DB::beginTransaction();
+		try {
 			$returnVal = parent::push();
-		});
+			DB::commit();
+		}
+		catch (\Exception $e) {
+			DB::rollBack();
+			throw $e;
+		}
 		return $returnVal;
 	}
 	
 	public function save(array $options = array()) {
 		$returnVal = NULL;
-		DB::transaction(function() use (&$options, &$returnVal) {
+		DB::beginTransaction();
+		try {
 			$returnVal = parent::save($options);
-		});
+			DB::commit();
+		}
+		catch (\Exception $e) {
+			DB::rollBack();
+			throw $e;
+		}
 		return $returnVal;
 	}
 }
