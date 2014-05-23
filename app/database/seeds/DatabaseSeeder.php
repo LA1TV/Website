b88d8968440892529b0d84ea483742c84350252f
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-23 17:42:32 +0100

    added warning if seeding on production server

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -8,6 +8,16 @@ class DatabaseSeeder extends Seeder {
 	 * @return void
 	 */
 	public function run() {
+		
+		if (App::environment() == "production") {
+			$this->command->info("WARNING: You are running in production. Data will be permanently deleted if you continue.");
+			if (!$this->command->confirm('Are you sure you want to continue? [y|n]: ', false))
+			{
+				$this->command->info("Aborting.");
+				return;
+			}
+		}
+		
 		$this->call('MediaItemSeeder');
 	}
 
