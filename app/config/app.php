1b9a9e08b1e2475df9a56747f6661e7cda1ed4d6
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-24 16:38:09 +0100

    installed asset-pipeline
    https://github.com/CodeSleeve/asset-pipeline

--- app/config/app.php
+++ app/config/app.php
@@ -119,7 +119,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-
+		
+		'Codesleeve\AssetPipeline\AssetPipelineServiceProvider',
 	),
 
 	/*
