419c30b9095476881bd2e68c9416ca50ce1028f0
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-25 12:22:20 +0100

    created VideoFiles seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('SiteUsersSeeder');
 		$this->call('FilesSeeder');
 		$this->call('MediaItemSeeder');
+		$this->call('VideoFilesSeeder');
 		$this->call('PlaylistsSeeder');
 	}
 
---
+++ app/database/seeds/VideoFilesSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use uk\co\la1tv\website\models\VideoFile;
+use uk\co\la1tv\website\models\MediaItemVideo;
+
+class VideoFilesSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		
+		// presumes that mediaitemvideo's have just been created with autoincrementing id
+		$mediaItemVideo = MediaItemVideo::find(1);
+		
+		$file = new VideoFile(array(
+			"width"		=>	1920,
+			"height"	=>	1080
+		));
+		$mediaItemVideo->videoFiles()->save($file);
+		
+		$file = new VideoFile(array(
+			"width"		=>	1280,
+			"height"	=>	720
+		));
+		$mediaItemVideo->videoFiles()->save($file);
+		
+		$file = new VideoFile(array(
+			"width"		=>	1024,
+			"height"	=>	576
+		));
+		$mediaItemVideo->videoFiles()->save($file);
+		
+		$file = new VideoFile(array(
+			"width"		=>	640,
+			"height"	=>	360
+		));
+		$mediaItemVideo->videoFiles()->save($file);
+		
+		$this->command->info('Video files records created and assigned to video media items!');
+	}
+
+
+}
