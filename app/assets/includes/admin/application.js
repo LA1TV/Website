231d93e71a06facec9b7bbd037a85b5b08ef6759
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-26 13:03:41 +0100

    installed jquery.fileupload plugin
    https://github.com/blueimp/jQuery-File-Upload

--- app/assets/includes/admin/application.js
+++ app/assets/includes/admin/application.js
@@ -5,4 +5,7 @@
 //
 //= require_tree jquery
 //= require bootstrap/js/bootstrap.js
+//= require jquery-file-upload/jquery.ui.widget.js
+//= require jquery-file-upload/jquery.iframe-transport.js
+//= require jquery-file-upload/jquery.fileupload.js
 //= require_tree ./custom
