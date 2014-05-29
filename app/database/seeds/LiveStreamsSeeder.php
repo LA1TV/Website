3db8ab942afaf1b09e027dfd22920c9db25af1bd
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-29 19:49:16 +0100

    updated livestreams seeder to create more streams

--- app/database/seeds/LiveStreamsSeeder.php
+++ app/database/seeds/LiveStreamsSeeder.php
@@ -13,60 +13,63 @@ class LiveStreamsSeeder extends Seeder {
 	 */
 	public function run() {
 		
-		DB::transaction(function() {
+		foreach(array(array("Studio Stream", "studio"), array("OB Stream 1", "ob1"), array("OB Stream 2", "ob2")) as $a) {
 		
-			$stream = new LiveStream(array(
-				"name"		=>	"Studio Stream",
-				"description"	=>	"The stream from the studio.",
-				"server_address"	=>	"la1tv-wowza1.lancs.ac.uk",
-				"dvr_enabled"	=>	false,
-				"stream_name"	=>	"studio",
-				"enabled"	=> false
-			));
-			$stream->save();
+			DB::transaction(function() use(&$a) {
 			
-			$pos = 0;
-			
-			$quality = new LiveStreamQuality(array(
-				"quality_id"	=> "auto",
-				"position"		=> $pos++
-			));
-			$quality->liveStream()->associate($stream);
-			$quality->qualityDefinition()->associate(QualityDefinition::find(7));
-			$quality->save();
-			
-			$quality = new LiveStreamQuality(array(
-				"quality_id"	=> "720p",
-				"position"		=> $pos++
-			));
-			$quality->liveStream()->associate($stream);
-			$quality->qualityDefinition()->associate(QualityDefinition::find(2));
-			$quality->save();
-			
-			$quality = new LiveStreamQuality(array(
-				"quality_id"	=> "360p",
-				"position"		=> $pos++
-			));
-			$quality->liveStream()->associate($stream);
-			$quality->qualityDefinition()->associate(QualityDefinition::find(4));
-			$quality->save();
-			
-			$quality = new LiveStreamQuality(array(
-				"quality_id"	=> "240p",
-				"position"		=> $pos++
-			));
-			$quality->liveStream()->associate($stream);
-			$quality->qualityDefinition()->associate(QualityDefinition::find(5));
-			$quality->save();
-			
-			$quality = new LiveStreamQuality(array(
-				"quality_id"	=> "160p",
-				"position"		=> $pos++
-			));
-			$quality->liveStream()->associate($stream);
-			$quality->qualityDefinition()->associate(QualityDefinition::find(6));
-			$quality->save();
-		});
+				$stream = new LiveStream(array(
+					"name"		=>	$a[0],
+					"description"	=>	NULL,
+					"server_address"	=>	"la1tv-wowza1.lancs.ac.uk",
+					"dvr_enabled"	=>	false,
+					"stream_name"	=>	$a[1],
+					"enabled"	=> false
+				));
+				$stream->save();
+				
+				$pos = 0;
+				
+				$quality = new LiveStreamQuality(array(
+					"quality_id"	=> "auto",
+					"position"		=> $pos++
+				));
+				$quality->liveStream()->associate($stream);
+				$quality->qualityDefinition()->associate(QualityDefinition::find(7));
+				$quality->save();
+				
+				$quality = new LiveStreamQuality(array(
+					"quality_id"	=> "720p",
+					"position"		=> $pos++
+				));
+				$quality->liveStream()->associate($stream);
+				$quality->qualityDefinition()->associate(QualityDefinition::find(2));
+				$quality->save();
+				
+				$quality = new LiveStreamQuality(array(
+					"quality_id"	=> "360p",
+					"position"		=> $pos++
+				));
+				$quality->liveStream()->associate($stream);
+				$quality->qualityDefinition()->associate(QualityDefinition::find(4));
+				$quality->save();
+				
+				$quality = new LiveStreamQuality(array(
+					"quality_id"	=> "240p",
+					"position"		=> $pos++
+				));
+				$quality->liveStream()->associate($stream);
+				$quality->qualityDefinition()->associate(QualityDefinition::find(5));
+				$quality->save();
+				
+				$quality = new LiveStreamQuality(array(
+					"quality_id"	=> "160p",
+					"position"		=> $pos++
+				));
+				$quality->liveStream()->associate($stream);
+				$quality->qualityDefinition()->associate(QualityDefinition::find(6));
+				$quality->save();
+			});
+		}
 		
 		$this->command->info('Live streams created and assigned qualities!');
 	}
