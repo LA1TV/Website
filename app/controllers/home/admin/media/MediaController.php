7f6801d7e549b4bc0ed89aa258cff5e9981e53d9
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-05-30 19:08:19 +0100

    fixed bug with checkboxes

--- app/controllers/home/admin/media/MediaController.php
+++ app/controllers/home/admin/media/MediaController.php
@@ -33,12 +33,12 @@ class MediaController extends MediaBaseController {
 		
 		// populate $formData with default values or received values
 		$formData = FormHelpers::getFormData(array(
-			array("enabled", ObjectHelpers::getProp(false, $mediaItem, "enabled")),
+			array("enabled", ObjectHelpers::getProp("y", $mediaItem, "enabled")),
 			array("name", ObjectHelpers::getProp("", $mediaItem, "name")),
 			array("description", ObjectHelpers::getProp("", $mediaItem, "description")),
 			array("cover-image-id", ObjectHelpers::getProp("", $mediaItem, "coverFile", "id")),
 			array("side-banners-image-id", ObjectHelpers::getProp("", $mediaItem, "sideBannersFile", "id")),
-			array("vod-enabled", ObjectHelpers::getProp(false, $mediaItem, "videoItem", "enabled")),
+			array("vod-enabled", ObjectHelpers::getProp("y", $mediaItem, "videoItem", "enabled")),
 			array("vod-name", ObjectHelpers::getProp("", $mediaItem, "videoItem", "name")),
 			array("vod-description", ObjectHelpers::getProp("", $mediaItem, "videoItem", "description")),
 			array("vod-video-id", ""), // TODO
@@ -46,14 +46,19 @@ class MediaController extends MediaBaseController {
 			array("vod-video-file-size", ""), //TODO
 			array("vod-time-recorded",  ObjectHelpers::getProp("", $mediaItem, "videoItem", "time_recorded")),
 			array("vod-publish-time", ObjectHelpers::getProp("", $mediaItem, "videoItem", "scheduled_publish_time")),
-			array("vod-live-recording", ObjectHelpers::getProp(false, $mediaItem, "videoItem", "is_live_recording")),
-			array("stream-enabled", ObjectHelpers::getProp(false, $mediaItem, "liveStreamItem", "enabled")),
+			array("vod-live-recording", ObjectHelpers::getProp("y", $mediaItem, "videoItem", "is_live_recording")),
+			array("stream-enabled", ObjectHelpers::getProp("y", $mediaItem, "liveStreamItem", "enabled")),
 			array("stream-name", ObjectHelpers::getProp("", $mediaItem, "liveStreamItem", "name")),
 			array("stream-description", ObjectHelpers::getProp("", $mediaItem, "liveStreamItem", "description")),
 			array("stream-live-time", ObjectHelpers::getProp("", $mediaItem, "liveStreamItem", "scheduled_live_time")),
 			array("stream-stream-id", ObjectHelpers::getProp(false, $mediaItem, "liveStreamItem", "liveStream", "id"))
 		), !$formSubmitted);
 		
+		// convert checkboxes values to booleans
+		$formData['enabled'] = $formData['enabled'] === "y";
+		$formData['vod-enabled'] = $formData['vod-enabled'] === "y";
+		$formData['vod-live-recording'] = $formData['vod-live-recording'] === "y";
+		$formData['stream-enabled'] = $formData['stream-enabled'] === "y";
 		
 		// now set filenames and sizes
 		$formData['cover-image-file-name'] = "";
