125a2456dbea0e4cf74653ce8190e3cbaf67f5eb
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-06-17 17:44:28 +0100

    removed code added for backwards compatibility
    php has now been upgraded

--- app/models/MyEloquent.php
+++ app/models/MyEloquent.php
@@ -8,36 +8,19 @@ class MyEloquent extends Eloquent {
 	
 	protected static $p = "uk\\co\\la1tv\\website\\models\\";
 	
-	// can't use DB::transaction() because cause php error on server when trying to access parent:: from closure
-	// Cannot access parent:: when no class scope is active
-	// error because php 5.4 adds feature to include $this and parent:: in closures (http://stackoverflow.com/q/23826810/1048589)
-	// leaving it like this for now because using package manager and that is not past 5.4 yet.
-	
 	public function push() {
 		$returnVal = NULL;
-		DB::beginTransaction();
-		try {
+		DB::transaction(function() use (&$returnVal) {
 			$returnVal = parent::push();
-			DB::commit();
-		}
-		catch (\Exception $e) {
-			DB::rollBack();
-			throw $e;
-		}
+		});
 		return $returnVal;
 	}
 	
 	public function save(array $options = array()) {
 		$returnVal = NULL;
-		DB::beginTransaction();
-		try {
+		DB::transaction(function() use (&$returnVal, &$options) {
 			$returnVal = parent::save($options);
-			DB::commit();
-		}
-		catch (\Exception $e) {
-			DB::rollBack();
-			throw $e;
-		}
+		});
 		return $returnVal;
 	}
 	
