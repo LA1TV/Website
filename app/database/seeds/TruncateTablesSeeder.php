692d17393768a61b128866650c87ded9c8d16e09
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-06-24 21:51:53 +0100

    updated seeder

--- app/database/seeds/TruncateTablesSeeder.php
+++ app/database/seeds/TruncateTablesSeeder.php
@@ -15,6 +15,7 @@ use uk\co\la1tv\website\models\SiteUser;
 use uk\co\la1tv\website\models\User;
 use uk\co\la1tv\website\models\VideoFile;
 use uk\co\la1tv\website\models\QualityDefinition;
+use uk\co\la1tv\website\models\VodVideoGroup;
 
 class TruncateTablesSeeder extends Seeder {
 
@@ -41,6 +42,7 @@ class TruncateTablesSeeder extends Seeder {
 		SiteUser::truncate();
 		User::truncate();
 		VideoFile::truncate();
+		VodVideoGroup::truncate();
 		DB::table("media_item_to_playlist")->truncate();
 		DB::table("permission_to_group")->truncate();
 		DB::table("user_to_group")->truncate();
