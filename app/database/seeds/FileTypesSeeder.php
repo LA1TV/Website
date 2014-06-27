df3163d34fabc48aac0890782fdb5979c4733ce5
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-06-27 23:35:46 +0100

    updated FileTypesSeeder

--- app/database/seeds/FileTypesSeeder.php
+++ app/database/seeds/FileTypesSeeder.php
@@ -16,10 +16,10 @@ class FileTypesSeeder extends Seeder {
 			array("id"=>2, "description"=>"For cover source images.", "extensions"=>array(1, 2, 3)),
 			array("id"=>3, "description"=>"For vod video source uploads.", "extensions"=>array(4)),
 			array("id"=>4, "description"=>"For cover art source images.", "extensions"=>array(1, 2, 3)),
-			array("id"=>5, "description"=>"For vod video renders.", "extensions"=>array(4)),
-			array("id"=>6, "description"=>"For side banner image renders.", "extensions"=>array(1)),
-			array("id"=>7, "description"=>"For cover image renders.", "extensions"=>array(1)),
-			array("id"=>8, "description"=>"For cover art image renders.", "extensions"=>array(1)),
+			array("id"=>5, "description"=>"For vod video renders.", "extensions"=>array()),
+			array("id"=>6, "description"=>"For side banner image renders.", "extensions"=>array()),
+			array("id"=>7, "description"=>"For cover image renders.", "extensions"=>array()),
+			array("id"=>8, "description"=>"For cover art image renders.", "extensions"=>array()),
 		);
 		
 		foreach($fileTypes as $a) {
