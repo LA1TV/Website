4175a9a78183440a737c0f54e66807a41d685643
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-07-11 19:50:50 +0100

    made page non user scaleable on mobile devices

--- app/views/layouts/home/admin/master.blade.php
+++ app/views/layouts/home/admin/master.blade.php
@@ -22,7 +22,7 @@ if (isset($nav[$currentNavPage])) {
 	<head>
 		<meta charset="utf-8">
 		<title><?=$title?></title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 		<meta name="description" content="">
 		<meta name="author" content="">
 		<meta name="robots" content="noindex, nofollow">
