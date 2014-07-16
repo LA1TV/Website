84bae1ad60d50f7f64d2ff25f522626b3b9f989c
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-07-16 19:59:43 +0100

    migration to add disabled to users table

---
+++ app/database/migrations/2014_07_16_195842_add disabled to users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDisabledToUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->boolean("disabled")->default(false);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn("disabled");
+		});
+	}
+
+}
