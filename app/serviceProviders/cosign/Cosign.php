e9dc736d560aadd5e256e45bd48cf2ebfad70832
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-07-23 21:52:37 +0100

    fix

--- app/serviceProviders/cosign/Cosign.php
+++ app/serviceProviders/cosign/Cosign.php
@@ -31,8 +31,8 @@ class Cosign {
 			dd("not found");
 			return;
 		}
-		if (preg_match("/^[A-Za-z0-9_\+\-/]+$/", $value) !== 1) {
-			// value contains unexpected characters
+		if (preg_match("/^[A-Za-z0-9_\+\-/]+$/", $key) !== 1) {
+			// key contains unexpected characters
 			dd("failed");
 			return;
 		}
