12d3ff5fc04a2aad4d05f54d02124cd4255e97a4
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-07-23 22:24:36 +0100

    fix

--- app/serviceProviders/auth/AuthManager.php
+++ app/serviceProviders/auth/AuthManager.php
@@ -214,7 +214,7 @@ class AuthManager {
 		
 		// http://www.lancaster.ac.uk/iss/tsg/cosign/using_php.html
 		$logoutUrl="https://weblogin.lancs.ac.uk/logout";
-		return Redirect::to($logoutUrl."?".$redirectUrl)->withCookie(Cookie::forget($_SERVER['COSIGN_SERVICE']));
+		return Redirect::to($logoutUrl."?".$redirectUrl)->withCookie(Cookie::forget(Config::get("auth.cosignServiceName")));
 	}
 	
 	// only allow a request once every $requestInterval seconds (with a bit of randomness) seconds for a particular user.
