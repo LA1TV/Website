99719d8d0dc7c74b027555c65e8810aed3895dd9
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-07-24 18:53:09 +0100

    formatting

--- app/views/home/admin/login/index.php
+++ app/views/home/admin/login/index.php
@@ -12,22 +12,22 @@
 		<h3>Login With Cosign</h3>
 		<p>Login with your university account details.</p>
 		<?php if ($cosignEnabled): ?>
-		<?=FormHelpers::getFormSubmitButton(2, "Login With Cosign", "", true);?>
+		<p><?=FormHelpers::getFormSubmitButton(2, "Login With Cosign", "", true);?></p>
 		<?php else: ?>
-		<button class="btn btn-default" type="button" disabled>Login With Cosign (Currently Unavailable)</button>
+		<p><button class="btn btn-default" type="button" disabled>Login With Cosign (Currently Unavailable)</button></p>
 		<?php endif; ?>
 		<h3>Login With Username and Password</h3>
 		<p>Login with a username and password assigned to you for this site. <strong>This is not your university login.</strong></p>
 		<?=FormHelpers::getFormTxtInput(1, "Username", "user", $form['user'], $formErrors);?>
 		<?=FormHelpers::getFormPassInput(1, "Password", "pass", $form['pass'], $formErrors);?>
-		<?=FormHelpers::getFormSubmitButton(1, "Login", "", true);?>
+		<p><?=FormHelpers::getFormSubmitButton(1, "Login", "", true);?></p>
 		<?php else: ?>
 		<h3>Logged In!</h3>
 		<?php if ($accountDisabled): ?>
 		<p>You are logged in but your account is currently disabled. Please contact an admin.</p>
 		<?php else: ?>
 		<p>You are logged in!</p>
-		<a href="<?=e(URL::to("/admin/dashboard"))?>" class="btn btn-primary">Click Here To Go To The Dashboard</a>
+		<p><a href="<?=e(URL::to("/admin/dashboard"))?>" class="btn btn-primary">Click Here To Go To The Dashboard</a></p>
 		<?php endif; ?>
 		<?php endif; ?>
 	</div>
