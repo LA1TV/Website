86e3c27def13749580b080f6113e98b5d22989f6
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-08-09 18:39:05 +0100

    removed Tom from config as no longer needed

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,6 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 	
 	'local' => array('homestead'),
-	'tom' => array('Tom-Laptop'),
 	'adam'=> array('AdamLaptop.local')
 
 ));
