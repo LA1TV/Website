2e4906ec2774dd92bfe5b43f298226e2ae22f9e0
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-09 15:03:55 +0100

    disabled cosign

--- app/config/auth.php
+++ app/config/auth.php
@@ -3,7 +3,7 @@
 return array(
 	// interval that must occur between login attempts in seconds
 	"attemptInterval"			=> 8,
-	"cosignEnabled"				=> true,
+	"cosignEnabled"				=> false,
 	"cosignServiceName"			=> "cosign-http-www2.la1tv.co.uk",
 	"cosignFilterDbLocation"	=> "/var/cosign/filter",
 	// time in seconds that cosign cookies are considered valid after being created before re-authentication needed
