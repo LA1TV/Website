4a80829170eb3f30728165fe385f641da339d654
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-14 10:35:03 +0100

    change so that facebook details can be ommited from .env files.

--- app/config/facebook.php
+++ app/config/facebook.php
@@ -2,8 +2,8 @@
 
 return array(
 	"enabled"			=> true,
-	"appId"				=> $_ENV['FACEBOOK_APP_ID'],
-	"appSecret"			=> $_ENV['FACEBOOK_APP_SECRET'],
+	"appId"				=> isset($_ENV['FACEBOOK_APP_ID']) ? $_ENV['FACEBOOK_APP_ID'] : null,
+	"appSecret"			=> isset($_ENV['FACEBOOK_APP_SECRET']) ? $_ENV['FACEBOOK_APP_SECRET'] : null,
 	// interval in minutes that must pass before the system checks facebook for updated profile info and whether token is still valid
 	"updateInterval"	=> 10
 );
