09588e58ef2ea55bbd6c2a80dc9965cc19cd73f6
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-19 17:14:23 +0100

    fix

--- app/assets/scripts/app/pages/home/playlist.js
+++ app/assets/scripts/app/pages/home/playlist.js
@@ -1,6 +1,6 @@
 define(["jquery", "lib/domReady!"], function($) {
 
-	$(".playlist-component").each(function() {
+	$(".playlist-element").each(function() {
 		var $table = $(this).find(".playlist-table").first();
 		// make the entire row a link to the item using the link on the thumbnail
 		$table.find("tbody").find("tr").each(function() {
