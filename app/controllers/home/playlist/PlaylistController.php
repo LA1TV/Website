3fa4b673669826de0d12cc75b911470bae565a87
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-20 23:03:56 +0100

    finished playlist page

--- app/controllers/home/playlist/PlaylistController.php
+++ app/controllers/home/playlist/PlaylistController.php
@@ -3,11 +3,61 @@
 use uk\co\la1tv\website\controllers\home\HomeBaseController;
 use View;
 use App;
+use uk\co\la1tv\website\models\Playlist;
 
 class PlaylistController extends HomeBaseController {
 
 	public function getIndex($id) {
-		$this->setContent(View::make("home.playlist.index"), "playlist", "playlist");
+		
+		$playlist = Playlist::with("show", "mediaItems", "relatedItems", "relatedItems.playlists")->accessible()->accessibleToPublic()->find(intval($id));
+		if (is_null($playlist)) {
+			App::abort(404);
+		}
+		
+		$playlistMediaItems = $playlist->mediaItems()->accessible()->orderBy("media_item_to_playlist.position")->get();
+		if (count($playlistMediaItems) === 0) {
+			App:abort(404);
+		}
+		
+		$playlistTableData = array();
+		$activeItemIndex = null;
+		foreach($playlistMediaItems as $i=>$item) {
+			$thumbnailUri = $playlist->getMediaItemCoverArtUri($item, 1920, 1080);
+			$playlistName = null;
+			if (is_null($playlist->show)) {
+				// this is a playlist not a series.
+				// show the series/playlist that each video in the playlist is from
+				$defaultPlaylist = $item->getDefaultPlaylist();
+				if (!is_null($defaultPlaylist->show)) {
+					// the current item in the playlist is part of a show.
+					$playlistName = $defaultPlaylist->generateName();
+				}
+			}
+			$playlistTableData[] = array(
+				"uri"					=> $playlist->getUri($item),
+				"title"					=> $item->name,
+				"description"			=> "Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description. Some really long description.",
+				"playlistName"			=> $playlistName,
+				"episodeNo"				=> intval($item->pivot->position) + 1,
+				"thumbnailUri"			=> $thumbnailUri
+			);
+		}
+		
+		$coverUri = null;
+		$coverFile = $playlist->coverFile;
+		if (!is_null($coverFile)) {
+			$coverFile = $coverFile->getImageFileWithResolution(940, 150);
+			if (!is_null($coverFile) && $coverFile->getShouldBeAccessible()) {
+				$coverUri = $coverFile->getUri();
+			}
+		}
+		
+		$view = View::make("home.playlist.index");
+		$view->playlistTitle = $playlist->generateName();
+		$view->playlistDescription = $playlist->description;
+		$view->coverImageUri = $coverUri;
+		$view->playlistTableData = $playlistTableData;
+		$this->setContent($view, "playlist", "playlist");
 	}
 	
 	public function missingMethod($parameters=array()) {
--- app/views/home/playlist/index.php
+++ app/views/home/playlist/index.php
@@ -1,5 +1,33 @@
-<div class="container">
-	<h1>Playlist</h1>
-	<p>The page for a specific playlist with links to media items.</p>
-	<p>/playlist/[media item id]</p>
+<?php if (!is_null($coverImageUri)): ?>
+<div class="cover-image-container">
+	<img class="img-responsive img-rounded" src="<?=e($coverImageUri);?>">
+</div>
+<?php endif; ?>
+<h1 class="no-top-margin"><?=e($playlistTitle);?></h1>
+<?php if (!is_null($playlistDescription)): ?>
+<p class="description"><?=e($playlistDescription);?></p>
+<?php endif; ?>
+<div class="playlist playlist-element">
+	<div class="playlist-table-container">
+		<table class="playlist-table table table-bordered table-striped table-hover">
+			<tbody>
+				<?php foreach($playlistTableData as $row):?>
+				<tr>
+					<td class="col-episode-no"><?=e($row['episodeNo'])?>.</td>
+					<td class="col-thumbnail"><a href="<?=e($row['uri']);?>"><img class="img-responsive" src="<?=e($row['thumbnailUri']);?>"/></a></td>
+					<?php if (is_null($row['playlistName'])): ?>
+					<td class="col-title"><?=e($row['title']);?></td>
+					<?php else: ?>
+					<td class="col-title clearfix">
+						<div class="subtitle"><span class="label label-info"><?=e($row['playlistName']);?></div></div>
+						<div class="title"><?=e($row['title']);?></div>
+						<?php if (!is_null($row['description'])): ?>
+						<div class="description"><?=e($row['description']);?></div>
+						<?php endif; ?>
+					</td>
+					<?php endif; ?>
+				</tr>
+				<?php endforeach; ?>
+		</table>
+	</div>
 </div>
