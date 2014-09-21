87e9663ec00aff8d822afbc1e98955cf1985a8bc
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-21 17:47:31 +0100

    New lines now ignored in description in playlist tables.

--- app/controllers/home/playlist/PlaylistController.php
+++ app/controllers/home/playlist/PlaylistController.php
@@ -34,7 +34,7 @@ class PlaylistController extends HomeBaseController {
 			$playlistTableData[] = array(
 				"uri"					=> $playlist->getUri($item),
 				"title"					=> $item->name,
-				"escapedDescription"	=> !is_null($item->description) ? nl2br(e($item->description)) : null,
+				"escapedDescription"	=> !is_null($item->description) ? e($item->description) : null,
 				"playlistName"			=> $playlistName,
 				"episodeNo"				=> intval($item->pivot->position) + 1,
 				"thumbnailUri"			=> $thumbnailUri,
@@ -70,7 +70,7 @@ class PlaylistController extends HomeBaseController {
 		
 		$view = View::make("home.playlist.index");
 		$view->playlistTitle = $playlist->generateName();
-		$view->escapedPlaylistDescription = !is_null($playlist->description) ? nl2br(URLHelpers::escapeAndReplaceUrls($playlist->description)) : null;
+		$view->escapedPlaylistDescription = !is_null($playlist->description) ? URLHelpers::escapeAndReplaceUrls($playlist->description) : null;
 		$view->coverImageUri = $coverUri;
 		$view->playlistTableFragment = count($playlistTableData) > 0 ? View::make("fragments.home.playlist", array(
 			"headerRowData"	=> null,
