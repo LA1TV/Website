b6825e31f1b3c9a45927324abe02fd938537dd1a
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-23 16:22:53 +0100

    Guide update

--- app/controllers/home/guide/GuideController.php
+++ app/controllers/home/guide/GuideController.php
@@ -18,10 +18,69 @@ class GuideController extends HomeBaseController {
 		
 		$mediaItems = MediaItem::accessible()->scheduledPublishTimeBetweenDates($startDate, $endDate)->whereHas("liveStreamItem", function($q2) {
 			$q2->accessible();
-		})->get();
+		})->orderBy("scheduled_publish_time", "asc")->orderBy("name", "asc")->orderBy("description", "asc")->get();
 		
-		dd($mediaItems->toArray());
+		// of form ("dateStr", "mediaItems")
+		$calendarData = array();
+		$previousMediaItem = null;
+		
+		foreach($mediaItems as $a) {
+			if (is_null($previousMediaItem) || $previousMediaItem->scheduled_publish_time->startOfDay()->timestamp !== $a->scheduled_publish_time->startOfDay()->timestamp) {
+				// new day
+				$calendarData[] = array(
+					"dateStr"		=> $a->scheduled_publish_time->startOfDay()->format('dS M'),
+					"mediaItems"	=> array()
+				);
+			}
+			$calendarData[count($calendarData)-1]['mediaItems'][] = $a;
+			$previousMediaItem = $a;
+		}
+		
+		$coverArtResolutions = Config::get("imageResolutions.coverArt");
+		$viewCalendarData = array();
+		foreach($calendarData as $day) {
+		
+			$playlistTableData = array();
+			
+			foreach($day['mediaItems'] as $i=>$item) {
+				$playlist = $item->getDefaultPlaylist();
+				$thumbnailUri = $playlist->getMediaItemCoverArtUri($item, $coverArtResolutions['thumbnail']['w'], $coverArtResolutions['thumbnail']['h']);
+				$playlistName = null;
+				if (!is_null($playlist->show)) {
+					// the current item in the playlist is part of a show.
+					$playlistName = $playlist->generateName();
+				}
+				$playlistTableData[] = array(
+					"uri"					=> $playlist->getMediaItemUri($item),
+					"title"					=> $playlist->generateEpisodeTitle($item),
+					"escapedDescription"	=> !is_null($item->description) ? e($item->description) : null,
+					"playlistName"			=> $playlistName,
+					// TODO: figure out how to get episode no
+					"episodeNo"				=> null,
+					"thumbnailUri"			=> $thumbnailUri,
+					"active"				=> false
+				);
+			}
+			
+			
+			
+		
+			$playlistFragment = View::make("fragments.home.playlist", array(
+				"headerRowData"	=> null,
+				"tableData"		=> $playlistTableData
+			));
+			
+			$viewCalendarData[] = array(
+				"dateStr"			=> $day['dateStr'],
+				"playlistFragment"	=> $playlistFragment
+			);
+		}
 	
-		$this->setContent(View::make("home.guide.index"), "guide", "guide");
+		
+		$view = View::make("home.guide.index");
+		$view->calendarData = $viewCalendarData;
+		$view->startDateStr = $startDate->format('dS M');
+		$view->endDateStr = $endDate->format('dS M');
+		$this->setContent($view, "guide", "guide");
 	}
 }
--- app/views/home/guide/index.php
+++ app/views/home/guide/index.php
@@ -1,3 +1,12 @@
-<h1>Guide</h1>
-<p>Table with upcoming live shows</p>
-<p>/guide</p>
+<h1 class="no-top-margin">Live Guide <small>(<?=e($startDateStr);?> - <?=e($endDateStr);?>)</small></h1>
+
+<table class="guide-table table table-bordered">
+	<tbody>
+<?php foreach($calendarData as $a): ?>
+		<tr>
+			<td class="col-date"><?=e($a['dateStr']);?></td>
+			<td class="col-item"><?=$a['playlistFragment']?></td>
+		</tr>
+<?php endforeach; ?>
+	</tbody>
+</table>
