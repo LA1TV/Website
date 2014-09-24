7aaeed6e0c948e65a936d8b4e9fe549132ef60d5
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-24 21:53:25 +0100

    finished contact page

--- app/views/home/contact/index.php
+++ app/views/home/contact/index.php
@@ -1,4 +1,11 @@
-<div class="container">
-	<h1>Contact</h1>
-	<p>/contact</p>
-</div>
+<h1>Contact Us</h1>
+<dl class="dl-horizontal contact-dl">
+	<dt>General Enquiries</dt>
+	<dd><a href="mailto:contact@la1tv.co.uk">contact@la1tv.co.uk</a></dd>
+	<dt>Technical Support</dt>
+	<dd><a href="mailto:development@la1tv.co.uk">development@la1tv.co.uk</a></dd>
+	<dt>Facebook</dt>
+	<dd><a href="https://www.facebook.com/LA1TV" target="_blank">https://www.facebook.com/LA1TV</a></dd>
+	<dt>Twitter</dt>
+	<dd><a href="https://twitter.com/LA1TV" target="_blank">https://twitter.com/LA1TV</a></dd>
+</dl>
