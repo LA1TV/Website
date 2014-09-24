48e9ef7ad0170ad83277466c48e0240647280db4
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-24 11:35:59 +0100

    Installed jquery slick plugin

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,25 @@
-<div class="container">
-	<h1>Index</h1>
+<h1>index</h1>
+
+<div class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+	</ol>
+	
+	<div class="carousel-inner">
+		<div class="item active">
+			<img src="..." alt="...">
+			<div class="carousel-caption">
+				...
+			</div>
+		</div>
+	</div>
+
+	<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+	</a>
+	<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+	</a>
 </div>
