9879c386fbc2b9e85842200826fbb39e90798c13
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-28 12:55:57 +0100

    trying to fix carousel

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,7 +4,9 @@
 	<div class="item">
 		<div class="embed-responsive embed-responsive-16by9">
 			<div class="content-container" data-jslink="<?=e($a['uri']);?>">
-				<img src="<?=e($a['coverArtUri']);?>" class="cover-img img-responsive">
+				<div class="bg">
+					<img src="<?=e($a['coverArtUri']);?>" class="cover-img img-responsive">
+				</div>
 				<div class="footer">
 					<div class="title"><?=e($a['name']);?></div>
 					<div class="available-msg"><?=e($a['availableMsg']);?></div>
