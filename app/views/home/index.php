1f067e95a88dfa9b8883832acb2e9d47c2e50c75
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-28 12:48:30 +0100

    trying to fix carousel flickering

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,13 +3,11 @@
 	<?php foreach($promotedItemsData as $a): ?>
 	<div class="item">
 		<div class="embed-responsive embed-responsive-16by9">
-			<div class="embed-responsive-item">
-				<div class="content-container" data-jslink="<?=e($a['uri']);?>">
-					<img src="<?=e($a['coverArtUri']);?>" class="cover-img img-responsive">
-					<div class="footer">
-						<div class="title"><?=e($a['name']);?></div>
-						<div class="available-msg"><?=e($a['availableMsg']);?></div>
-					</div>
+			<div class="content-container" data-jslink="<?=e($a['uri']);?>">
+				<img src="<?=e($a['coverArtUri']);?>" class="cover-img img-responsive">
+				<div class="footer">
+					<div class="title"><?=e($a['name']);?></div>
+					<div class="available-msg"><?=e($a['availableMsg']);?></div>
 				</div>
 			</div>
 		</div>
