972dd1d9361f20efd53c3af8db6bbe6036f3b658
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-09-29 09:47:33 +0100

    fix

--- app/controllers/home/HomeController.php
+++ app/controllers/home/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends HomeBaseController {
 			$promotedItemsData[] = array(
 				"coverArtUri"	=> $a['coverArtUri'],
 				"name"			=> $mediaItem->name,
-				"seriesName"	=> $a['seriesName'],
+				"seriesName"	=> null,
 				"availableMsg"	=> $liveNow ? "Live Now!" : $this->buildTimeStr($isLiveShow, $mediaItem->scheduled_publish_time),
 				"uri"			=> $a['uri']
 			);
