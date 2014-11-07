f17547579f4fb896d218fc2873f6fb7c6af2ec09
Tom Jenkinson <tjcode@outlook.com>
2014-11-07 13:57:29 +0000

    fix which prevented home page showing when no carousel
    previously if the carousel was now shown (because no items for it) the
    rest of the page below the nav bar would never fade in.

--- app/assets/scripts/app/pages/home/home-page.js
+++ app/assets/scripts/app/pages/home/home-page.js
@@ -6,49 +6,57 @@ define([
 	
 	$(".page-home").first().each(function() {
 		
+		function animateInPage() {
+			$pageContainer.animate({opacity: 1}, 500);
+		}
+		
 		var $pageContainer = $(this).first();
 		
 		$pageContainer.css("display", "block");
 		
-		$pageContainer.find(".promo-carousel").each(function() {
-			var self = this;
-			
-			function animateFooter(show, footerPos) {
-				$footers[footerPos].animate({opacity: show ? 1 : 0}, aniDuration/2);
-			}
-		
-			$(this).css("display", "block");
-			
-			var $footers = [];
-			
-			$(this).find(".footer").each(function() {
-				$footers.push($(this).first());
-				if ($footers.length === 1) {
-					$(this).css("opacity", 1);
+		var $promoCarousel = $pageContainer.find(".promo-carousel").first();
+		if ($promoCarousel.length > 0) {
+			$promoCarousel.each(function() {
+				var self = this;
+				
+				function animateFooter(show, footerPos) {
+					$footers[footerPos].animate({opacity: show ? 1 : 0}, aniDuration/2);
 				}
+			
+				$(this).css("display", "block");
+				
+				var $footers = [];
+				
+				$(this).find(".footer").each(function() {
+					$footers.push($(this).first());
+					if ($footers.length === 1) {
+						$(this).css("opacity", 1);
+					}
+				});
+				
+				var aniDuration = 800;
+				
+				var slick = $(this).slick({
+					dots: true,
+					autoplay: true,
+					autoplaySpeed: 4500,
+					arrows: true,
+					fade: false,
+					pauseOnHover: true,
+					speed: aniDuration,
+					onBeforeChange: function() {
+						animateFooter(false, slick.slickCurrentSlide());
+					},
+					onAfterChange: function() {
+						animateFooter(true, slick.slickCurrentSlide());
+					}
+				});
+				animateInPage();
 			});
-			
-			var aniDuration = 800;
-			
-			var slick = $(this).slick({
-				dots: true,
-				autoplay: true,
-				autoplaySpeed: 4500,
-				arrows: true,
-				fade: false,
-				pauseOnHover: true,
-				speed: aniDuration,
-				onBeforeChange: function() {
-					animateFooter(false, slick.slickCurrentSlide());
-				},
-				onAfterChange: function() {
-					animateFooter(true, slick.slickCurrentSlide());
-				}
-			});
-			
-			$pageContainer.animate({opacity: 1}, 500);
-			
-		});
+		}
+		else {
+			animateInPage();
+		}
 		
 	});
 	
