d63fb605f3150ecfe7a7c1050debd751e969f264
Tom Jenkinson <tjcode@outlook.com>
2014-11-09 22:00:45 +0000

    changed text

--- app/views/home/account/index.php
+++ app/views/home/account/index.php
@@ -1,7 +1,7 @@
 <h1>Account Settings</h1>
 <?php if ($loggedIn): ?>
 <h2>Facebook Notifications</h2>
-<p>Enabling this allows us to send you facebook notifications. For example whenever new content is ready to watch or a show goes live.</p>
+<p>Enabling this allows us to send you facebook notifications. For example whenever new content becomes available or a show goes live.</p>
 <div class="facebook-notifications-button-group-container" data-buttonsdata="<?=e(json_encode($facebookNotificationsButtonsData));?>" data-chosenid="<?=e($facebookNotificationsButtonsInitialId);?>"></div>
 <h2>Logout</h2>
 <a class="btn btn-default" href="<?=e($logoutUri);?>" data-confirm="Are you sure you want to log out of the website?">Click Here To Logout</a>
