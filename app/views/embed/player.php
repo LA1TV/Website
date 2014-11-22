1cd4ca5b2cfd704d3ee98ed87c27fefa3e08d445
Tom Jenkinson <tjcode@outlook.com>
2014-11-22 11:45:22 +0000

    Can now add "showheading=0" to embed url to hide heading

--- app/views/embed/player.php
+++ app/views/embed/player.php
@@ -1,4 +1,5 @@
 <div class="heading-container clearfix">
+<?php if ($showHeading): ?>
 	<div class="logo-container">
 		<?php if (!$hasVideo): ?>
 		<a href="<?=e($hyperlink);?>" target="_blank"><img class="img-responsive" src="<?=asset("assets/img/logo.png");?>"/></a>
@@ -11,6 +12,7 @@
 		<h1 class="episode-title">Content Unavailable</h1>
 		<?php endif; ?>
 	</div>
+<?php endif; ?>
 </div>
 <?php if ($hasVideo): ?>
 <div class="player-container-component-container" data-info-uri="<?=e($playerInfoUri);?>" data-register-view-count-uri="<?=e($registerViewCountUri);?>" data-login-required-msg="<?=e($loginRequiredMsg);?>" data-enable-admin-override="<?=$adminOverrideEnabled?"1":"0"?>" data-register-like-uri="<?=e($registerLikeUri);?>">
