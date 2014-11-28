c4428f62e23824a344d4e57e2a6db6f1cb7097d9
Tom Jenkinson <tjcode@outlook.com>
2014-11-28 19:10:06 +0000

    Added maintenence mode page
    triggered with php artisan down command

---
+++ app/views/layouts/maintenance/body.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base')
+
+@section('body')
+<?=$content?>
+@stop
---
+++ app/views/maintenance.php
@@ -0,0 +1,8 @@
+<div class="container main-content">
+	<div class="logo-container">
+		<img class="img-responsive logo" alt="LA1:TV Logo" src="<?=asset("/assets/img/logo.png");?>"/>
+	</div>
+	<h1>Down For Maintenance</h1>
+	<p class="lead">Sorry we are currently down for maintenance. Please check back later!</p>
+	<p>For more information please contact the development team at "<a href="mailto:<?=e($developmentEmail);?>"><?=e($developmentEmail);?></a>".</p>
+</div>
