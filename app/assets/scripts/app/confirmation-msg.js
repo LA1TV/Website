9ce7e9accc8c542948285decf369859bc6f7c8c2
Tom Jenkinson <tjcode@outlook.com>
2014-12-17 16:04:47 +0000

    Added confirmation message to logout button.

--- app/assets/scripts/app/confirmation-msg.js
+++ app/assets/scripts/app/confirmation-msg.js
@@ -1,8 +1,12 @@
-define(["jquery", "lib/domReady!"], function($) {
+define([
+	"jquery",
+	"app/page-protect",
+	"lib/domReady!"
+], function($, PageProtect) {
 
 	$("a[data-confirm]").click(function() {
 		if (confirm($(this).attr("data-confirm"))) {
-			pageProtect.disable();
+			PageProtect.disable();
 			return true;
 		}
 		return false;
