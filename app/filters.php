d5ee3b739b30ce621ff37f660442eb839b5cc83a
Tom Jenkinson <tjcode@outlook.com>
2015-01-13 17:44:25 +0000

    csp header now puts stream server domains in default-src not media-src

--- app/filters.php
+++ app/filters.php
@@ -118,6 +118,6 @@ Route::filter('setContentSecurityPolicyHeader', function($route, $request, $resp
 		}
 		$domainsString = implode(" ", $allowedDomains);
 	
-		$response->header("Content-Security-Policy-Report-Only", "default-src 'self'; media-src 'self' ".$domainsString."; script-src 'self' https://www.google-analytics.com; img-src *; frame-src 'none'");
+		$response->header("Content-Security-Policy-Report-Only", "default-src 'self' ".$domainsString."; media-src 'self'; script-src 'self' https://www.google-analytics.com; img-src *; frame-src 'none'");
 	}
 });
