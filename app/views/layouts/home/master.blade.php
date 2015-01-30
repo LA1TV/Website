ee771ba698e9604e209471a255816c9279ac3416
Tom Jenkinson <tjcode@outlook.com>
2015-01-30 13:19:19 +0000

    shows and playlists dropdown now becomes link instead if dropdown would be empty

--- app/views/layouts/home/master.blade.php
+++ app/views/layouts/home/master.blade.php
@@ -5,30 +5,34 @@
 @stop
 
 @section('navbarList')
+<?php if (count($showsDropdown) > 0): ?>
 <li class="dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Shows <b class="caret"></b></a>
 	<ul class="dropdown-menu">
 	<?php foreach($showsDropdown as $a): ?>
 		<li><a href="<?=e($a['uri']);?>"><?=e($a['text']);?></a></li>
 	<?php endforeach; ?>
-	<?php if (count($showsDropdown) > 0): ?>
 		<li class="divider"></li>
-	<?php endif; ?>
 		<li><a href="<?=e($showsUri);?>">View All</a></li>
 	</ul>
 </li>
+<?php else: ?>
+<li><a href="<?=e($showsUri);?>">Shows</a></li>
+<?php endif; ?>
+<?php if (count($playlistsDropdown) > 0): ?>
 <li class="dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Playlists <b class="caret"></b></a>
 	<ul class="dropdown-menu">
 		<?php foreach($playlistsDropdown as $a): ?>
 		<li><a href="<?=e($a['uri']);?>"><?=e($a['text']);?></a></li>
 	<?php endforeach; ?>
-	<?php if (count($playlistsDropdown) > 0): ?>
 		<li class="divider"></li>
-	<?php endif; ?>
 		<li><a href="<?=e($playlistsUri);?>">View All</a></li>
 	</ul>
 </li>
+<?php else: ?>
+<li><a href="<?=e($playlistsUri);?>">Playlists</a></li>
+<?php endif; ?>
 <li><a href="<?=e($guideUri);?>">Live Guide</a></li>
 <li><a href="<?=e($blogUri);?>">Blog</a></li>
 <li><a href="<?=e($contactUri);?>">Contact</a></li>
