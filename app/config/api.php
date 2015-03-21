22669a2433af7552766804cd44367d256ea15f9a
Tom Jenkinson <tjcode@outlook.com>
2015-03-21 15:31:00 +0000

    changed comment wording

--- app/config/api.php
+++ app/config/api.php
@@ -2,6 +2,6 @@
 
 return array(
 	// if false then the api will return a service unavailable response.
-	// this should be enabled in the "local" config folder
+	// this should be overridden in the "local" config folder
 	"enabled"		=> false
 );
