d205d6a7026fe50cc1cf4bffc9775b0299b492a4
unknown <tjcode@outlook.com>
2015-05-26 18:22:25 +0100

    API transformer fix

--- app/api/transformers/Transformer.php
+++ app/api/transformers/Transformer.php
@@ -4,6 +4,9 @@ abstract class Transformer {
 
 	// the $options array will be passed to every item
 	public function transformCollection(array $items, array $options=[]) {
+		if (count($items) === 0) {
+			return [];
+		}
 		return array_map([$this, "transform"], $items, array_fill(0, count($items), $options));
 	}
 	
