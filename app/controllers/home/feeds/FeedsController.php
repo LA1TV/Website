fbaee240d697a6bb9f3c26a3653d2426d182aad5
Tom Jenkinson <tjcode@outlook.com>
2015-09-29 19:05:20 +0100

    Most recent content rss feed now generated

--- app/controllers/home/feeds/FeedsController.php
+++ app/controllers/home/feeds/FeedsController.php
@@ -3,6 +3,8 @@
 use uk\co\la1tv\website\controllers\BaseController;
 use Feed;
 use Config;
+use Carbon;
+use URL;
 use uk\co\la1tv\website\models\MediaItem;
 
 class FeedsController extends BaseController {
@@ -12,24 +14,25 @@ class FeedsController extends BaseController {
 		$feed = Feed::make();
 		
 		// cache response for a minute
-		$feed->setCache(Config::get("custom.feeds_cache_time", 'feeds-latest');
-	
+		$feed->setCache(Config::get("custom.feeds_cache_time", 'feeds-latest'));
+		
 		if (!$feed->isCached()) {
 			$items = MediaItem::getCachedRecentItems();
 			$feed->title = "LA1:TV's Latest Content";
 			$feed->description = 'This feed contains the latest content that is published to our website.';
 			$feed->logo = asset("/assets/img/logo.png");
-			$feed->link = URL::to('feeds-latest'); // TODO maybe this should be to the homepage?
-			$feed->setDateFormat('carbon');
-			$feed->pubdate = TODO; // TODO
+			$feed->link = URL::route('home');
+			$feed->setDateFormat('datetime');
+			$feed->pubdate = Carbon::now();
 			$feed->lang = 'en';
 			$feed->setShortening(false);
-			foreach ($posts as $post) {
-				// set item's title, author, url, pubdate, description and content
-				// TODO
-				//$feed->add($post->title, $post->author, URL::to($post->slug), $post->created, $post->description, $post->content);
+			foreach ($items as $item) {
+				$mediaItem = $item['mediaItem'];
+				$scheduledPublishTime = new Carbon($mediaItem->scheduled_publish_time);
+				// title, author, url, pubdate, description
+				$feed->add($item['generatedName'], "LA1:TV", $item['uri'], $scheduledPublishTime, $mediaItem->description);
 			}
-
+		}
+		return $feed->render('rss');
 	}
-	return $feed->render('rss');
 }
