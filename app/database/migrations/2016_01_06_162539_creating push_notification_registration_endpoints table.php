8d1cbc70d6f61cdeb40e8b06992eb14ad9856b63
Tom Jenkinson <tjcode@outlook.com>
2016-01-06 16:32:16 +0000

    Created push_notification_registration_endpoints table

---
+++ app/database/migrations/2016_01_06_162539_creating push_notification_registration_endpoints table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatingPushNotificationRegistrationEndpointsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('push_notification_registration_endpoints', function(Blueprint $table)
+		{
+			$table->string("session_id", 255);
+			$table->string('url');
+			
+			$table->foreign("session_id")->references('id')->on('sessions')->onUpdate("restrict")->onDelete('cascade');
+			
+			$table->primary(array('session_id'));
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('push_notification_registration_endpoints');
+	}
+
+}
