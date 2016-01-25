6732f959c9635b44404d57b3571179c267f33bbf
Tom Jenkinson <tjcode@outlook.com>
2016-01-25 19:29:45 +0000

    Disabled csp headers because currently incompatible.

--- app/helpers/MyResponse.php
+++ app/helpers/MyResponse.php
@@ -2,10 +2,16 @@
 
 class MyResponse extends Illuminate\Http\Response {
 	
-	private $contentSecurityPolicyEnabled = true;
+	// cannot be enabled at the moment as the site puts the page config as an inline
+	// script now for performance. A hash of this needs adding to the script tag
+	// and the appropriate header in order for this to be possible again
+	private $contentSecurityPolicyEnabled = false;
 	private $contentSecurityPolicyDomains = array();
 	
 	public function enableContentSecurityPolicy($enable) {
+		if ($enable) {
+			throw(new Exception("This cannot be enabled at the moment."));
+		}
 		$this->contentSecurityPolicyEnabled = (boolean) $enable;
 	}
 	
