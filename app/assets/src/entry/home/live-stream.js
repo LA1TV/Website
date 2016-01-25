eb20ef39eceee1f2605ec8a77475b85295995992
Tom Jenkinson <tjcode@outlook.com>
2016-01-25 20:14:55 +0000

    Added missing css import to live streams entry point

--- app/assets/src/entry/home/live-stream.js
+++ app/assets/src/entry/home/live-stream.js
@@ -1,3 +1,4 @@
 require("app/pages/home/global");
 require("app/pages/home/live-stream-page.css");
 require("app/pages/home/live-stream-page");
+require("app/player-container-component-container.css");
