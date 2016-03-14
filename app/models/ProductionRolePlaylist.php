c0de3793e026d05bf0bc18609097509f95f15fd3
Tom Jenkinson <tjcode@outlook.com>
2016-03-14 23:13:48 +0000

    Added search() to ProductionRolePlaylist

--- app/models/ProductionRolePlaylist.php
+++ app/models/ProductionRolePlaylist.php
@@ -16,4 +16,22 @@ class ProductionRolePlaylist extends MyEloquent {
 	public function getDescription() {
 		return !is_null($this->description_override) ? $this->description_override : $this->productionRole->description;
 	}
+
+	public function scopeSearch($q, $value) {
+		if ($value === "") {
+			return $q;
+		}
+		
+		// if name is overridden here, then search the name here,
+		// otherwise search the name on the ProductionRole model
+		return $q->where(function($q2) use (&$value) {
+			$q2->where(function($q3) use (&$value) {
+				$q3->whereNotNull("name_override")->whereContains(array("name_override"), $value);
+			})->orWhere(function($q3) use (&$value) {
+				$q3->whereNull("name_override")->whereHas("productionRole", function($q4) use (&$value) {
+					$q4->whereContains(array("name"), $value);
+				});
+			});
+		});
+	}
 }
