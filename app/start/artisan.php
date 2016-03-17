f575b443a16230c1975457cd8e15d79621db1a56
Tom Jenkinson <tjcode@outlook.com>
2016-03-17 19:04:37 +0000

    Session garbage collection now handled in cron job
    Fixes #820

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -15,6 +15,7 @@ use uk\co\la1tv\website\commands\RecentItemsCacheUpdateCommand;
 use uk\co\la1tv\website\commands\PromotedItemsCacheUpdateCommand;
 use uk\co\la1tv\website\commands\LiveItemsCacheUpdateCommand;
 use uk\co\la1tv\website\commands\ClearTempChunksCommand;
+use uk\co\la1tv\website\commands\ClearOldSessionsCommand;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,3 +44,4 @@ Artisan::add(new RecentItemsCacheUpdateCommand());
 Artisan::add(new PromotedItemsCacheUpdateCommand());
 Artisan::add(new LiveItemsCacheUpdateCommand());
 Artisan::add(new ClearTempChunksCommand());
+Artisan::add(new ClearOldSessionsCommand());
