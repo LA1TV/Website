01390e9861857365722100eaf6750165a3c9b91d
Josh Hodgson <josh.pbh@gmail.com>
2016-04-08 19:43:02 +0100

    Enable 'native' mobile web apps
    This means apps when 'added to home screen' on Android of iOS will open as if they are a native app, without opening the browser or having the address bar.
    
    This is fine, because you can't get into any dead ends on the site. Also, all 'external' referrals open in the native browser

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -5,6 +5,8 @@
 		<meta charset="utf-8">
 		<title><?=$title?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="mobile-web-app-capable" content="yes">
 		<?php if (isset($description)): ?>
 		<meta name="description" content="<?=e($description)?>">
 		<?php endif; ?>
