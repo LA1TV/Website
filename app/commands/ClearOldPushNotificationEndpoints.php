792ac02dcc75944056f63d44d373c6de09be167b
Tom Jenkinson <tjcode@outlook.com>
2016-08-16 22:34:56 +0100

    Created ClearOldPushNotificationEndpoints job

---
+++ app/commands/ClearOldPushNotificationEndpoints.php
@@ -0,0 +1,88 @@
+<?php namespace uk\co\la1tv\website\commands;
+
+use Indatus\Dispatcher\Scheduling\ScheduledCommand;
+use Indatus\Dispatcher\Scheduling\Schedulable;
+use Indatus\Dispatcher\Drivers\Cron\Scheduler;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Config;
+use DebugHelpers;
+use uk\co\la1tv\website\models\PushNotificationRegistrationEndpoint;
+
+class ClearOldPushNotificationEndpointsCommand extends ScheduledCommand {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'clearOldPushNotificationEndpoints:run';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Remove old push notification endpoints.';
+	
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	 /**
+     * When a command should run
+     *
+     * @param Scheduler $scheduler
+     * @return \Indatus\Dispatcher\Scheduling\Schedulable
+     */
+	public function schedule(Schedulable $scheduler)
+	{
+		return $scheduler->everyHours(12);
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		if (!DebugHelpers::shouldSiteBeLive()) {
+			$this->info('Not running because site should not be live at the moment.');
+			return;
+		}
+
+		$this->info('Removing old push notification endpoints.');
+
+		$cutOffTime = time() - (Config::get("pushNotifications.lifetime")*60);
+		PushNotificationRegistrationEndpoint::where('updated_at', '<=', $cutOffTime)->delete();
+		$this->info("Finished.");
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array();
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array();
+	}
+
+}
--- app/config/pushNotifications.php
+++ app/config/pushNotifications.php
@@ -7,6 +7,8 @@ return array(
 	// if null push notifications won't work for chrome browsers
 	"gcmApiKey"		=> isset($_ENV['GCM_API_KEY']) ? $_ENV['GCM_API_KEY'] : null,
 	"gcmProjectNumber"	=> isset($_ENV['GCM_PROJECT_NUMBER']) ? $_ENV['GCM_PROJECT_NUMBER'] : "745046379475",
+	// endpoint urls that aren't updated in this amount of time will be removed
+	"lifetime" => 201600, // 20 weeks
 	// only endpoints which start with the following will be accepted
 	"endpointWhiteList" => array(
 		"https://android.googleapis.com/gcm/send/",
