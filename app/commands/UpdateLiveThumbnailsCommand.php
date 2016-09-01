7fcc9f2c6402ad5b635ca95bfef949983f98456b
Tom Jenkinson <tjcode@outlook.com>
2016-09-01 13:58:41 +0100

    Implement UpdateLiveThumbnailsCommand

---
+++ app/commands/UpdateLiveThumbnailsCommand.php
@@ -0,0 +1,137 @@
+<?php namespace uk\co\la1tv\website\commands;
+
+use Indatus\Dispatcher\Scheduling\ScheduledCommand;
+use Indatus\Dispatcher\Scheduling\Schedulable;
+use Indatus\Dispatcher\Drivers\Cron\Scheduler;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use uk\co\la1tv\website\models\LiveStreamUri;
+use DebugHelpers;
+use LiveThumbnails;
+use Redis;
+use Config;
+
+class UpdateLiveThumbnailsCommand extends ScheduledCommand {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'liveThumbnails:update';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Starts/stops live thumbnail generaton for live streams.';
+	
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	 /**
+     * When a command should run
+     *
+     * @param Scheduler $scheduler
+     * @return \Indatus\Dispatcher\Scheduling\Schedulable
+     */
+	public function schedule(Schedulable $scheduler)
+	{
+		// default is run every minute
+		return $scheduler;
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		if (!DebugHelpers::shouldSiteBeLive()) {
+			$this->info('Not running because site should not be live at the moment.');
+			return;
+		}
+
+		$liveThumbnailsServiceUri = Config::get("serviceUri");
+		if (is_null($liveThumbnailsServiceUri)){
+			$this->info("Live thumbnails disabled.");
+			return;
+		}
+
+		if (Redis::get("liveThumbnailsUpdateRunning")) {
+			$this->info('Update already in progress.');
+			return;
+		}
+		Redis::set("liveThumbnailsUpdateRunning", true, "EX", 300);
+		
+		$this->info('Updating live thumbnails.');
+
+		$liveStreamUris = LiveStreamUri::with("liveStream")->get();
+		foreach($liveStreamUris as $liveStreamUri) {
+			$thumbnailsEnabled = 
+				!is_null($liveStreamUri->thumbnails_source_uri) &&
+				$liveStreamUri->enabled &&
+				$liveStreamUri->has_dvr &&
+				$liveStreamUri->liveStream->enabled &&
+				$liveStreamUri->liveStream->shown_as_livestream;
+			if (!$thumbnailsEnabled) {
+				if (!is_null($liveStreamUri->thumbnails_id)) {
+					$id = $liveStreamUri->thumbnails_id;
+					$liveStreamUri->thumbnails_id = null;
+					$liveStreamUri->thumbnails_manifest_uri = null;
+					$liveStreamUri->save();
+					LiveThumbnails::stopGenerator($id);
+				}
+			}
+			else {
+				$this->info("Updating for live steam uri with ID ".$liveStreamUri->id);
+				if (is_null($liveStreamUri->thumbnails_id) || !LiveThumbnails::checkStillRunning($liveStreamUri->thumbnails_id)) {
+					// it's stopped so start it again
+					$liveStreamUri->thumbnails_id = null;
+					$liveStreamUri->thumbnails_manifest_uri = null;	
+					// TODO prevent duplicates
+					$result = LiveThumbnails::startGenerator($liveStreamUri->thumbnails_source_uri);
+					if (!is_null($result)) {
+						$id = $result["id"];
+						$manifestUri = $result["manifestUri"];
+						$liveStreamUri->thumbnails_id = $id;
+						$liveStreamUri->thumbnails_manifest_uri = $manifestUri;
+					}
+					$liveStreamUri->save();
+				}					
+			}
+		}
+		Redis::del("liveThumbnailsUpdateRunning");
+		$this->info("Finished.");
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array();
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array();
+	}
+
+}
--- app/config/liveThumbnails.php
+++ app/config/liveThumbnails.php
@@ -3,5 +3,9 @@
 return array(
 	// the url to the service running https://github.com/tjenkinson/hls-live-thumbnails
 	// live thumbnails disabled if null
-	"liveThumbnailsServiceUri"	=> isset($_ENV['LIVE_THUMBNAILS_SERVICE_URI']) ? $_ENV['LIVE_THUMBNAILS_SERVICE_URI'] : null
+	"serviceBaseUri"	=> isset($_ENV['LIVE_THUMBNAILS_SERVICE_BASE_URI']) ? $_ENV['LIVE_THUMBNAILS_SERVICE_BASE_URI'] : null,
+	// the base url that the thumbnails will be served at e.g. http://example.com/thumbnails
+	"publicBaseUri"	=> isset($_ENV['LIVE_THUMBNAILS_PUBLIC_BASE_URI']) ? $_ENV['LIVE_THUMBNAILS_PUBLIC_BASE_URI'] : null,
+	// the secret if configured in the service
+	"secret" => isset($_ENV['LIVE_THUMBNAILS_SERVICE_SECRET']) ? $_ENV['LIVE_THUMBNAILS_SERVICE_SECRET'] : null
 );
